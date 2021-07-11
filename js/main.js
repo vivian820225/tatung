cbf77c6f11737b0073d98f3aa0d7a88aeb0ec00b
Vivian Chiang <vivian820225@gmail.com>
2021-07-11 10:03:33 +0800

    [Fix] 調整樣式

--- js/main.js
+++ js/main.js
@@ -1,50 +1,3 @@
-function mediaCheck() {
-  const desktopGroupSlides = document.querySelectorAll(".group_slide");
-  const mobileGroupSlides = document.querySelectorAll(".m_group_slide");
-  desktopGroupSlides.forEach((dGroupSlide) => {
-    if (innerWidth <= 768) {
-      dGroupSlide.classList.remove("slide");
-    } else {
-      dGroupSlide.classList.add("slide");
-    }
-  });
-  mobileGroupSlides.forEach((mGroupSlide) => {
-    if (innerWidth <= 768) {
-      mGroupSlide.classList.add("slide");
-    } else {
-      mGroupSlide.classList.remove("slide");
-    }
-  });
-}
-function onImageLoaded(url, cb) {
-  let image = new Image();
-  image.src = url;
-  if (image.complete) {
-    cb(image);
-  } else {
-    image.onload = function () {
-      cb(image);
-    };
-  }
-}
-function checkImgOnload() {
-  const panoramaSection = document.querySelector(".panorama-container");
-  let image = new Image();
-  image.onload = function () {
-    onImageLoaded(`${image.src}`, function () {
-      panoramaSection.innerHTML = `<img src="${image.src}" data-width="${image.width}" data-height="${image.height}" alt="Panorama">`;
-    });
-    $(".panorama-view").panorama360({
-      sliding_controls: false,
-      bind_resize: true,
-    });
-  };
-  if (innerWidth <= 768) {
-    image.src = "images/mobile/s1/sec1img01.jpg";
-  } else {
-    image.src = "images/desktop/s1/sec1img01.jpg";
-  }
-}
 function init() {
   $("#fullpage").fullpage({
     anchors: [
@@ -58,9 +11,6 @@ function init() {
       "8thPage",
       "9thPage",
       "10thPage",
-      "11thPage",
-      "12thPage",
-      "13thPage",
     ],
     slidesNavigation: true,
     loopHorizontal: false,
@@ -73,14 +23,5 @@ function init() {
 }
 
 $(document).ready(function () {
-  // checkImgOnload();
-  // mediaCheck();
   init();
-
-  // window.addEventListener("resize", function () {
-  //   setTimeout(function () {
-  //     this.location.reload();
-  //   });
-  // });
-  
 });
